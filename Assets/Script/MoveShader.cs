5b1b008a12311212a23ee3644510b1f14b06b784
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-03-07 15:08:38 +0100

    Coroutine + prefab

--- Assets/Script/MoveShader.cs
+++ Assets/Script/MoveShader.cs
@@ -7,14 +7,30 @@ public class MoveShader : MonoBehaviour
 
     [SerializeField] private ParticleSystem _ps;
     [SerializeField] private AnimationCurve _width;
-    [SerializeField] private AnimationCurve _heigth;
+    [SerializeField] private AnimationCurve _height;
     
     [ContextMenu("test")]
     public void Launch()
     {
         ParticleSystemRenderer pr = (ParticleSystemRenderer)_ps.GetComponent<Renderer>();
+        VaryShaderPropertiesOverTime(pr);
+    }
+    
+    private IEnumerator VaryShaderPropertiesOverTime(ParticleSystemRenderer a)
+    {
+        float elapsedTime = 0f;
+        float duration = 2f; // Durée totale de la variation, ajustez selon vos besoins.
+
+        while (elapsedTime < duration)
+        {
+            float normalizedTime = elapsedTime / duration;
+            a.material.SetFloat("_Width", _width.Evaluate(normalizedTime));
+            a.material.SetFloat("_Height", _height.Evaluate(normalizedTime));
+            yield return null;
+            elapsedTime += Time.deltaTime;
+        }
         
-        pr.material.SetFloat("_Width", _width.Evaluate(0.5f));
-        pr.material.SetFloat("_Hidth", _heigth.Evaluate(0.5f));
+        a.material.SetFloat("_Width", _width.Evaluate(1f));
+        a.material.SetFloat("_Hidth", _height.Evaluate(1f));
     }
 }
