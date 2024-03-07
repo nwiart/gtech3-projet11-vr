6baa340552bddafafaf3ea1c972fbbb9ce04e451
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-03-07 14:24:02 +0100

    Fix scale of paper and v1 lithgning bolt

--- Assets/Script/MoveShader.cs
+++ Assets/Script/MoveShader.cs
@@ -9,16 +9,12 @@ public class MoveShader : MonoBehaviour
     [SerializeField] private AnimationCurve _width;
     [SerializeField] private AnimationCurve _heigth;
     
-
     [ContextMenu("test")]
     public void Launch()
     {
         ParticleSystemRenderer pr = (ParticleSystemRenderer)_ps.GetComponent<Renderer>();
-        //coroutine qui varie l'apaisseur et la loongueur du shader.
         
         pr.material.SetFloat("_Width", _width.Evaluate(0.5f));
-        pr.material.SetFloat("_Width", _heigth.Evaluate(0.5f));
+        pr.material.SetFloat("_Hidth", _heigth.Evaluate(0.5f));
     }
-
-
 }
