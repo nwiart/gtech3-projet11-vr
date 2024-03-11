642eed5ba507c62c07066292a555e53e3f202e98
ataillat <ataillat@gaming.tech>
2024-03-11 17:22:47 +0100

    à tester

--- Assets/Scripts/Skills/SkillFireBender.cs
+++ Assets/Scripts/Skills/SkillFireBender.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
 
-public class SkillFireBender : Skill
+public class SkillFireBender : MonoBehaviour
 {
     [SerializeField] GameObject fireBallPrefab; // Préfabriqué de la boule de feu
-    [SerializeField] float fireBallSpeed = 10f; // Vitesse de la boule de feu
+    [SerializeField] Transform firePoint; // Point de départ du tir de la boule de feu
+    [SerializeField] float fireBallSpeed = 1f; // Vitesse de la boule de feu
 
-    // Fonction pour tirer la boule de feu
-    public void Fire(Transform skillTransform)
+    public void Fire()
     {
-        // Instancier une boule de feu à partir du préfabriqué
-        GameObject fireBall = Instantiate(fireBallPrefab, skillTransform.position, Quaternion.identity);
+        GameObject newObject = Instantiate(fireBallPrefab, firePoint.position, firePoint.rotation, null);
 
-        // Récupérer le rigidbody de la boule de feu
-        Rigidbody rb = fireBall.GetComponent<Rigidbody>();
+        if (newObject.TryGetComponent(out Rigidbody rigidBody))
+        {
+            ApplyForce(rigidBody);
+        }
+    }
 
-        // Vérifier si le rigidbody existe
-        if (rb != null)
-        {
-            // Appliquer une force à la boule de feu pour la faire avancer en ligne droite
-            rb.velocity = skillTransform.forward * fireBallSpeed;
-        }
-        else
-        {
-            Debug.LogError("Le préfabriqué de la boule de feu ne contient pas de Rigidbody.");
-        }
+    void ApplyForce(Rigidbody rigidBody)
+    {
+        Vector3 force = firePoint.forward * fireBallSpeed;
+        rigidBody.AddForce(force);
     }
 }
