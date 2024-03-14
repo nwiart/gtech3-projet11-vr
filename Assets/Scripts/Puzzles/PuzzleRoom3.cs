b3baff176abc8197e5291f7866077932c04c9088
unknown <nwiart@gamingcampus.local>
2024-03-14 12:55:55 +0100

    LES CABLES

--- Assets/Scripts/Puzzles/PuzzleRoom3.cs
+++ Assets/Scripts/Puzzles/PuzzleRoom3.cs
@@ -1,57 +1,157 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class PuzzleRoom3 : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _objectsInOrder;
+    [Serializable]
+    public struct CableConnector
+    {
+        [SerializeField] public XRSocketInteractor Socket;
 
-    [SerializeField] private XRSocketInteractor[] _socketsInOrder;
+        [SerializeField] public int SectionFrom;
+        [SerializeField] public int SectionTo;
 
-    private GameObject[] _currentObjects;
+
+        public GameObject Cable;
+    }
+
+    [SerializeField] private GameObject[] _cableSections;
+
+    [SerializeField] private CableConnector[] _sockets;
+
+
+    [SerializeField] private Material _litCableMaterial;
 
 
     public UnityEvent OnSolve;
-    public UnityEvent Thunder;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        _currentObjects = new GameObject[_socketsInOrder.Length];
+        _sockets[0].Cable = null;
+        _sockets[0].Socket.selectEntered.AddListener((args) => { OnAttach(0, args); });
+        _sockets[0].Socket.selectExited.AddListener((args) => { OnDetach(0, args); });
 
-        _socketsInOrder[0].selectEntered.AddListener((args) => { OnAttach(0, args); });
-        _socketsInOrder[1].selectEntered.AddListener((args) => { OnAttach(1, args); });
-        _socketsInOrder[2].selectEntered.AddListener((args) => { OnAttach(2, args); });
-        _socketsInOrder[3].selectEntered.AddListener((args) => { OnAttach(3, args); });
-        _socketsInOrder[4].selectEntered.AddListener((args) => { OnAttach(4, args); });
+        _sockets[1].Cable = null;
+        _sockets[1].Socket.selectEntered.AddListener((args) => { OnAttach(1, args); });
+        _sockets[1].Socket.selectExited.AddListener((args) => { OnDetach(1, args); });
+
+        _sockets[2].Cable = null;
+        _sockets[2].Socket.selectEntered.AddListener((args) => { OnAttach(2, args); });
+        _sockets[2].Socket.selectExited.AddListener((args) => { OnDetach(2, args); });
+
+        _sockets[3].Cable = null;
+        _sockets[3].Socket.selectEntered.AddListener((args) => { OnAttach(3, args); });
+        _sockets[3].Socket.selectExited.AddListener((args) => { OnDetach(3, args); });
+
+        _sockets[4].Cable = null;
+        _sockets[4].Socket.selectEntered.AddListener((args) => { OnAttach(4, args); });
+        _sockets[4].Socket.selectExited.AddListener((args) => { OnDetach(4, args); });
+
+        CheckCurrent();
     }
     
-    private void CheckObjects()
+
+    private int FindConnector(int from, int to)
     {
-        bool good = true;
-        for (int i = 0; i < _currentObjects.Length; i++)
+        for (int i = 0; i < _sockets.Length; i++)
         {
-            if (_currentObjects[i] != _objectsInOrder[i])
+            if (_sockets[i].SectionFrom == from && _sockets[i].SectionTo == to) return i;
+        }
+        return -1;
+    }
+
+
+    private void LightCable(List<MeshRenderer> cables, GameObject cable)
+    {
+        MeshRenderer r = cable.GetComponent<MeshRenderer>();
+        if (r != null)
+        {
+            cables.Add(r);
+        }
+    }
+
+    private void LightCables(List<MeshRenderer> cables, GameObject section)
+    {
+        List<GameObject> l = new List<GameObject>();
+        section.GetChildGameObjects(l);
+
+        foreach (GameObject c in l)
+        {
+            MeshRenderer r = c.GetComponent<MeshRenderer>();
+            if (r != null)
             {
-                good = false;
-                break;
+                cables.Add(r);
             }
         }
+    }
 
-        if (good)
+    private void CheckCurrent()
+    {
+        List<MeshRenderer> cablesToLight = new List<MeshRenderer>();
+
+        int con;
+
+        LightCables(cablesToLight, _cableSections[0]);
+
+        con = FindConnector(0, 1);
+        if (con != -1 && _sockets[con].Cable != null)
         {
+            LightCable(cablesToLight, _sockets[con].Cable);
+            LightCables(cablesToLight, _cableSections[1]);
+        }
+        else goto light;
+
+        con = FindConnector(1, 2);
+        if (con != -1 && _sockets[con].Cable != null)
+        {
+            LightCable(cablesToLight, _sockets[con].Cable);
+            LightCables(cablesToLight, _cableSections[2]);
+        }
+        else goto light;
+
+        con = FindConnector(1, 3);
+        if (con != -1 && _sockets[con].Cable != null)
+        {
+            LightCable(cablesToLight, _sockets[con].Cable);
+            LightCables(cablesToLight, _cableSections[3]);
+        }
+        else goto light;
+
+        con = FindConnector(3, 4);
+        if (con != -1 && _sockets[con].Cable != null)
+        {
+            LightCable(cablesToLight, _sockets[con].Cable);
+            LightCables(cablesToLight, _cableSections[4]);
+
             OnSolve.Invoke();
+        }
+        else goto light;
+
+    light:
+        foreach (MeshRenderer r in cablesToLight)
+        {
+            r.material = _litCableMaterial;
         }
     }
 
     private void OnAttach(int i, SelectEnterEventArgs args)
     {
-        _currentObjects[i] = args.interactable.gameObject;
+        Debug.Log(i);
+        _sockets[i].Cable = args.interactable.gameObject;
 
-        CheckObjects();
+        CheckCurrent();
     }
-    
+
+    private void OnDetach(int i, SelectExitEventArgs args)
+    {
+        Debug.Log(i);
+        _sockets[i].Cable = null;
+    }
 }
