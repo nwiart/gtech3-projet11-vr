d1b8bb4d6243e7a622408c7b193266f040500efe
unknown <nwiart@gamingcampus.local>
2024-03-14 11:24:23 +0100

    _tmp

--- Assets/Scripts/ScriptableObjects/Skills/SkillLightningBender.cs
+++ Assets/Scripts/ScriptableObjects/Skills/SkillLightningBender.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 
 [CreateAssetMenu(menuName = "Skills/Lightning Bender")]
 public class SkillLightningBender : Skill
 {
+    [SerializeReference]
+    private GameObject _lightningBoltPrefab;
+
+
     public override void OnUnlock(Player player)
     {
-
     }
 
     public override void SimpleActivate(Player p)
     {
-        throw new System.NotImplementedException();
+        GameObject _flame = Instantiate(_lightningBoltPrefab);
+        _flame.transform.SetParent(p.RightHand.transform, false);
     }
 
     public override void SpellSphere()
     {
-        throw new System.NotImplementedException();
+        
     }
 }
