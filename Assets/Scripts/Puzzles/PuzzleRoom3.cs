80ba830693a6527e79362a81c3b9f299a8b8ecab
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2024-03-14 11:08:11 +0100

    prefab 3 modif et debut puzzel room 3

---
+++ Assets/Scripts/Puzzles/PuzzleRoom3.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PuzzleRoom3 : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _objectsInOrder;
+
+    [SerializeField] private XRSocketInteractor[] _socketsInOrder;
+
+    private GameObject[] _currentObjects;
+
+
+    public UnityEvent OnSolve;
+    public UnityEvent Thunder;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentObjects = new GameObject[_socketsInOrder.Length];
+
+        _socketsInOrder[0].selectEntered.AddListener((args) => { OnAttach(0, args); });
+        _socketsInOrder[1].selectEntered.AddListener((args) => { OnAttach(1, args); });
+        _socketsInOrder[2].selectEntered.AddListener((args) => { OnAttach(2, args); });
+        _socketsInOrder[2].selectEntered.AddListener((args) => { OnAttach(3, args); });
+        _socketsInOrder[2].selectEntered.AddListener((args) => { OnAttach(4, args); });
+    }
+    
+    private void CheckObjects()
+    {
+        bool good = true;
+        for (int i = 0; i < _currentObjects.Length; i++)
+        {
+            if (_currentObjects[i] != _objectsInOrder[i])
+            {
+                good = false;
+                break;
+            }
+        }
+
+        if (good)
+        {
+            OnSolve.Invoke();
+        }
+    }
+
+    private void OnAttach(int i, SelectEnterEventArgs args)
+    {
+        _currentObjects[i] = args.interactable.gameObject;
+
+        CheckObjects();
+    }
+    
+}
